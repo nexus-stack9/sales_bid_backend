f89d5b31f366fea3f9dfb1f48997930db7291908
dev-neeraj96 <neerajbatchu96@gmail.com>
2025-06-27 23:52:54 +0530

    changes for my bids

--- src/routes/BidsRoute.js
+++ src/routes/BidsRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { insertBid } = require('../controller/BidController');
+const { insertBid, userBids } = require('../controller/BidController');
 
 // Route to insert a new bid
 router.post('/placeBid', insertBid);
+router.get('/userBids/:bidder_id', userBids);
 
 module.exports = router;
