d33d630c63861106594b284347ef747c54d6efb5
Prakash <prakash.c1996@gmail.com>
2025-10-13 22:20:32 -0700

    pos changes

---
+++ src/routes/PosRoute.js
@@ -0,0 +1,15 @@
+const express  = require('express');
+const router = express.Router();
+
+
+
+const { getAllProductsByVendorId,getAllVendor,getVendorByStatus,getVendorById,updateVendorStatus } = require('../controller/posController');
+
+// Route to get all products by vendor ID
+router.get('/getAllProductsByVendorId/:vendorId', getAllProductsByVendorId);
+router.get('/getAllVendor', getAllVendor);
+router.get('/getVendorByStatus/:status', getVendorByStatus);
+router.get('/getVendorById/:vendorId', getVendorById);
+router.put('/updateVendorStatus/:vendorId/status/:status', updateVendorStatus);
+
+module.exports = router;
